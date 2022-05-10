cae29ff606e6cd87f85a60aff9a69bb844957482
GrazKev <graciaskevin99@gmail.com>
2022-05-10 14:38:36 +0700

    Jadi

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter'],
+        poppins: ['Poppins'],
+      },
+    },
+  },
+  plugins: [],
+};
